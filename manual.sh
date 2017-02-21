5bc74bc17b52455d8207933118d6457e29943cb8
Taylor Rainwater <rainwatertr@appstate.edu>
2017-02-20 23:37:44 -0500

    Updated everything

--- manual.sh
+++ manual.sh
@@ -1,15 +1,9 @@
 #!/bin/bash
+
 # the_manual - a simple script to convert all base man
 #	           pages into pdfs 
 # author: Rev Taylor R Rainwater
 #   date: 23-03-2016
-######################################################
-# TODO
-# - fix file counter
-# - give more notifications
-# - place txt and pdf files in their respective 
-#   directories
-
 
 # the various directories which contain the system 
 # commands
@@ -21,64 +15,59 @@ u_sbin="/usr/sbin/$1"
 let count=0
 
 manual_process() {
-	for f in $1/*
-	do
-		echo "THE MANUAL SYSTEM INDEPENDENT COMPILER"
-		echo "======================================"
-		echo "PROCESSED: " $(basename $f)
-		man $(basename $f) | col -bx > $2$(basename $f).txt
-		enscript -Bhqp $2$(basename $f).ps $2$(basename $f).txt
-		yes | rm $2$(basename $f).txt
-		clear
-		let count=count+1
-	done
+    for f in $1/*
+    do
+	echo "PROCESSED: " $(basename $f)
+	man $(basename $f) | col -bx > $(basename $f).txt
+	enscript -Bhqp $2$(basename $f).ps $(basename $f).txt
+	yes | rm $(basename $f).txt
+	let count=count+1
+    done
 }
 
 manual_merge() {
-	echo "  MERGING: " $1
-	gs -sDEVICE=pdfwrite \
-	-dNOPAUSE -dBATCH -dSAFER -q \
-	-sOutputFile=$1.pdf $1/*.ps
+    gs -sDEVICE=pdfwrite \
+       -dNOPAUSE -dBATCH -dSAFER -q \
+       -sOutputFile=$1.pdf $1/*.ps
 }
 
 manual_build() {
-	echo " BUILDING: the_manual.pdf"
-	gs -sDEVICE=pdfwrite \
-	-dNOPAUSE -dBATCH -dSAFER -q \
-	-sOutputFile=the_manual.pdf *.pdf
+    gs -sDEVICE=pdfwrite \
+       -dNOPAUSE -dBATCH -dSAFER -q \
+       -sOutputFile=the_manual.pdf *.pdf
 }
 
 manual_clean() {
-	echo "CLEANING UP"
-	yes | rm r_bin/*.ps &
-	yes | rm r_sbin/*.ps &
-	yes | rm u_bin/*.ps &
-	yes | rm u_sbin/*.ps &
-	yes | rm r_bin.pdf r_sbin.pdf u_bin.pdf u_sbin.pdf &
-	wait 
+    rm -f r_bin/*.ps &
+    rm -f r_sbin/*.ps &
+    rm -f u_bin/*.ps &
+    rm -f u_sbin/*.ps &
+    rm -f r_bin.pdf r_sbin.pdf u_bin.pdf u_sbin.pdf &
+    wait 
 }
 
 manual_main() {
-	# Create all ps files from the man pages
-	manual_process $r_bin r_bin/ &
-	manual_process $r_sbin r_sbin/ &
-	manual_process $u_bin u_bin/ &
-	manual_process $u_sbin u_sbin/ &
-	wait
-	# Merge all ps files from their sections
-	# to a pdf with the name of the section
-	echo "=============================="
-	manual_merge r_bin &
-	manual_merge r_sbin &
-	manual_merge u_bin &
-	manual_merge u_sbin &
-	wait
-	# Merge the four sections
-	echo "=============================="
-	manual_build
+    # Create all ps files from the man pages
+    echo "!--BEGINNING--!"
+    manual_process $r_bin r_bin/ &
+    manual_process $r_sbin r_sbin/ &
+    manual_process $u_bin u_bin/ &
+    manual_process $u_sbin u_sbin/ &
+    wait
+    # Merge all ps files from their sections
+    # to a pdf with the name of the section
+    echo "!--MERGING--!"
+    manual_merge r_bin &
+    manual_merge r_sbin &
+    manual_merge u_bin &
+    manual_merge u_sbin &
+    wait
+    # Merge the four sections
+    echo "!--BUILDING--!"
+    manual_build
     manual_clean
-	echo "TOTAL FILES PROCESSED: " $count
-	echo "OUTPUT FILE IS the_manual.pdf"
+    echo "TOTAL FILES PROCESSED: " $count
+    echo "OUTPUT FILE IS the_manual.pdf"
 }
 manual_main 2> the_manual.log
 
